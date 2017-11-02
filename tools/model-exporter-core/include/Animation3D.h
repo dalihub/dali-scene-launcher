af9f5a989a009f0efdde8af711d8672f7a2052bd
György Straub <g.straub@partner.samsung.com>
2017-11-02 16:58:26 +0000

    model-exporter update. - Fixed the writing of the mesh URI in SaveScene() on *nices. - Split model-exporter into model-exporter-core (static library) and   model-exporter-cli projects - Added model-exporter-jni - Added com.samsung.dali.modelConverter RCP projects, which implement   a GUI to model-exporter.
    Change-Id: I763732e6e381089c1773a8708cbdb1a50199d996
    Signed-off-by: György Straub <g.straub@partner.samsung.com>

---
+++ tools/model-exporter-core/include/Animation3D.h
@@ -0,0 +1,49 @@
+#ifndef ANIMATION3D_H
+#define ANIMATION3D_H
+#include <string>
+#include <vector>
+
+struct NodeKey
+{
+    NodeKey()
+    {
+        time = v[0] = v[1] = v[2] = v[3] = 0.0f;
+    }
+    float time;
+    //v[3] is w in quaternion and 0.0 for vector3
+    float v[4];
+};
+
+class NodeAnimation3D
+{
+    public:
+        NodeAnimation3D();
+        virtual ~NodeAnimation3D();
+
+        std::string NodeName;
+        std::vector<NodeKey> Rotations;
+        std::vector<NodeKey> Positions;
+        std::vector<NodeKey> Scales;
+    protected:
+
+    private:
+
+};
+
+class Animation3D
+{
+    public:
+        Animation3D();
+        virtual ~Animation3D();
+        bool HasAnimations();
+        float TicksPerSecond;
+        float Duration;
+        std::string Name;
+        std::vector<NodeAnimation3D> AnimNodesList;
+    protected:
+
+    private:
+
+};
+
+#endif // ANIMATION3D_H
