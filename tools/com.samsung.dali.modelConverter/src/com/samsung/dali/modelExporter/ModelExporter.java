0833a7a68a90132de95792c29c0a14f2851698e5
György Straub <g.straub@partner.samsung.com>
2017-11-08 11:27:27 +0000

    Added functionality to modelConverter to run commands and get their output in the text element of MainPart.
    - added LoggingProcessRunner,
    - added support to set busy state of the main UI,
    - added support clear and append to, the contents of the main text field,
    - added a File menu with the following options:
      - restarting the SDB daemon,
      - deploying RPM and TPK packages,
      - exiting the application.
    - cleaned up the visibility and naming of class members and added the license to the header.
    
    Change-Id: I1eda93f138f11732bbf9736c9819d8f75e26dcc2

--- tools/com.samsung.dali.modelConverter/src/com/samsung/dali/modelExporter/ModelExporter.java
+++ tools/com.samsung.dali.modelConverter/src/com/samsung/dali/modelExporter/ModelExporter.java
@@ -1,9 +1,26 @@
 package com.samsung.dali.modelExporter;
 
+/*
+ * Copyright (c) 2017 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 public class ModelExporter {
 
 	private static boolean s_initialised = false;
-	
+
 	public static void initialise()
 	{
 		if(!s_initialised)
@@ -12,7 +29,7 @@ public class ModelExporter {
 			s_initialised = true;
 		}
 	}
-	
+
 	/**
 	 * @brief Performs the export.
 	 * @param inputFile - the .dae file to process. Required.
@@ -21,7 +38,7 @@ public class ModelExporter {
 	 * @return 0 on success, 1 on failure.
 	 */
 	public static native int nativeExport(String inputFile, String outputName);
-	
+
 	/**
 	 * @return An explanation on what might have gone wrong, if anything has.
 	 * 			Null string if everything is fine.
