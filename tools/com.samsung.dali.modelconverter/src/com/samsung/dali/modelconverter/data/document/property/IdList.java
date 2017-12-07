faab904166a7b1c8f5996dda5aa72286e15156f0
György Straub <g.straub@partner.samsung.com>
2017-12-07 17:43:14 +0000

    Added the Property system and NodePropertiesPart.java
    - Added GlobalParts.createPart() to facilitate the instantiation of parts prior to them
      being used, in case they weren't created (shown) yet.
    - Added widgets for the presentation of properties: Id-, Text-, and TransformPropertyWidget.
    - Added human-readable identifiers for Meshes, Materials, Shaders.
    - Added (some) matrix decomposition functionality to MatrixHelper.
    - Implemented Property.Provider in Node, Camera and Scene.
    - Changed the parts to tabs with indication to what they're supposed to present.
    
    Change-Id: Id11fd5b2b6c9dc17e1268c5c073a57233c2afb73

---
+++ tools/com.samsung.dali.modelconverter/src/com/samsung/dali/modelconverter/data/document/property/IdList.java
@@ -0,0 +1,48 @@
+package com.samsung.dali.modelconverter.data.document.property;
+
+/*
+ * Copyright (c) 2017 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import com.samsung.dali.modelconverter.data.document.property.Property.IValueProvider;
+
+public class IdList<T> implements IValueProvider {
+
+  public IdList(List<T> values, boolean nullable) {
+    mValues = new ArrayList<String>();
+    if (nullable) {
+      mValues.add("(none)");
+    }
+    for (Object o : values) {
+      mValues.add(o.toString());
+    }
+  }
+
+  public IdList(List<T> values) {
+    this(values, false);
+  }
+
+  @Override
+  public Collection<?> getValues() {
+    return mValues;
+  }
+
+  List<String> mValues;
+}
--- tools/com.samsung.dali.modelconverter/src/com/samsung/dali/modelconverter/view/parts/GlobalParts.java
+++ tools/com.samsung.dali.modelconverter/src/com/samsung/dali/modelconverter/view/parts/GlobalParts.java
@@ -1,13 +1,61 @@
 package com.samsung.dali.modelconverter.view.parts;
 
+/*
+ * Copyright (c) 2017 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+import org.eclipse.e4.core.contexts.EclipseContextFactory;
+import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.e4.ui.workbench.modeling.EPartService;
+import org.eclipse.e4.ui.workbench.modeling.EPartService.PartState;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+
 public class GlobalParts {
 
   public static DliTextPart getDliTextPart() {
+    if (DliTextPart.sActiveInstance == null) {
+      createPart("com.samsung.dali.modelconverter.part.dlitext");
+      assert DliTextPart.sActiveInstance != null;
+    }
     return DliTextPart.sActiveInstance;
   }
 
   public static SceneGraphPart getSceneGraphPart() {
+    if (SceneGraphPart.sActiveInstance == null) {
+      createPart("com.samsung.dali.modelconverter.part.scenegraph");
+      assert SceneGraphPart.sActiveInstance != null;
+    }
     return SceneGraphPart.sActiveInstance;
   }
 
+  public static NodePropertiesPart getNodePropertiesPart() {
+    if (NodePropertiesPart.sActiveInstance == null) {
+      createPart("com.samsung.dali.modelconverter.part.nodeproperties");
+      assert NodePropertiesPart.sActiveInstance != null;
+    }
+    return NodePropertiesPart.sActiveInstance;
+  }
+
+  static void createPart(String id) {
+    Bundle bundle = FrameworkUtil.getBundle(EPartService.class);
+    BundleContext bundleContext = bundle.getBundleContext();
+    IEclipseContext eclipseContext = EclipseContextFactory.getServiceContext(bundleContext);
+    EPartService partService = (EPartService) eclipseContext.get(EPartService.class);
+    partService.showPart(id, PartState.CREATE);
+  }
 }
