46e3bf421a062de15aa55d04a32ff04083955588
György Straub <g.straub@partner.samsung.com>
2017-12-07 17:43:14 +0000

    Added the Property system and NodePropertiesPart.java
    - Added GlobalParts.createPart() to facilitate the instantiation of parts prior to them
      being used, in case they weren't created (shown) yet.
    - Added widgets for the presentation of properties: Id-, Text-, and TransformPropertyWidget.
    - Added human-readable identifiers for Meshes, Materials, Shaders.
    - Added (some) matrix decomposition functionality to MatrixHelper.
    - Implemented Property.Provider in Node, Camera and Scene.
    - Changed the parts to tabs with indication to what they're supposed to present.
    
    Change-Id: Id11fd5b2b6c9dc17e1268c5c073a57233c2afb73

---
+++ tools/com.samsung.dali.modelconverter/src/com/samsung/dali/modelconverter/controller/PropertiesPresenter.java
@@ -0,0 +1,89 @@
+package com.samsung.dali.modelconverter.controller;
+
+/*
+ * Copyright (c) 2017 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Collection;
+
+import org.eclipse.swt.SWT;
+
+import com.samsung.dali.modelconverter.data.document.MatrixHelper;
+import com.samsung.dali.modelconverter.data.document.property.Property;
+import com.samsung.dali.modelconverter.view.parts.PropertiesPart;
+import com.samsung.dali.modelconverter.view.widgets.IdPropertyWidget;
+import com.samsung.dali.modelconverter.view.widgets.TextPropertyWidget;
+import com.samsung.dali.modelconverter.view.widgets.TransformPropertyWidget;
+
+public class PropertiesPresenter implements Property.IReceiver {
+
+  PropertiesPresenter(PropertiesPart part) {
+    mPart = part;
+  }
+
+  @Override
+  public void resetProperties() {
+    mPart.resetProperties();
+  }
+
+  @Override
+  public void register(String name, Property property) {
+    int style = SWT.NONE;
+    Collection<?> values = property.getRange();
+    try {
+      switch (property.getType()) {
+      case Integer:
+        // TODO: enable editing
+        new TextPropertyWidget(mPart.getComposite(), style).setWritable(false)
+          .setValue(Integer.toString((Integer) property.get())).setName(name);
+        break;
+      case Number:
+        // TODO: enable editing
+        new TextPropertyWidget(mPart.getComposite(), style).setWritable(false)
+          .setValue(Double.toString((Double) property.get())).setName(name);
+        break;
+      case String:
+        // TODO: enable editing
+        new TextPropertyWidget(mPart.getComposite(), style).setWritable(false)
+            .setValue((String) property.get()).setName(name);
+        break;
+      case Transform:
+        double[] value = (double[]) property.get();
+        double[] t = MatrixHelper.getTranslation(value);
+        double[] s = MatrixHelper.getScale(value);
+        double[] r = MatrixHelper.getRotation(value);
+        // TODO: enable editing
+        new TransformPropertyWidget(mPart.getComposite(), style).setValue(t, s, r).setWritable(false).setName(name);
+        break;
+      case Id:
+        // TODO: enable editing
+        new IdPropertyWidget(mPart.getComposite(), style).setWritable(false).setRange(values)
+            .setSelection((Integer) property.get()).setName(name);
+      default:
+        break;
+      }
+    }
+    catch (IllegalArgumentException | IllegalAccessException | InvocationTargetException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
+    mPart.getComposite().requestLayout();
+  }
+
+  PropertiesPart mPart;
+}
--- tools/com.samsung.dali.modelconverter/src/com/samsung/dali/modelconverter/data/document/Node.java
+++ tools/com.samsung.dali.modelconverter/src/com/samsung/dali/modelconverter/data/document/Node.java
@@ -1,5 +1,22 @@
 package com.samsung.dali.modelconverter.data.document;
 
+/*
+ * Copyright (c) 2017 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 
@@ -7,8 +24,11 @@ import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSetter;
+import com.samsung.dali.modelconverter.data.document.property.IdList;
+import com.samsung.dali.modelconverter.data.document.property.NullOffsetter;
+import com.samsung.dali.modelconverter.data.document.property.Property;
 
-public class Node {
+public class Node implements Property.IProvider {
 
   @Override
   public String toString() {
@@ -29,7 +49,7 @@ public class Node {
     return mMaterialId;
   }
 
-  public void setMaterialId(int id) {
+  public void setMaterialId(Integer id) {
     mMaterialId = id;
   }
 
@@ -42,7 +62,7 @@ public class Node {
     return mShaderId;
   }
 
-  public void setShaderId(int id) {
+  public void setShaderId(Integer id) {
     mShaderId = id;
   }
 
@@ -55,7 +75,7 @@ public class Node {
     return mMeshId;
   }
 
-  public void setMeshId(int id) {
+  public void setMeshId(Integer id) {
     mMeshId = id;
   }
 
@@ -95,6 +115,24 @@ public class Node {
   @JsonGetter("matrix")
   public double[] getMatrix() {
     return mMatrix;
+  }
+
+  @Override
+  public void provideProperties(Document context, Property.IReceiver receiver) {
+    try {
+      receiver.register("Name", new Property(this, "Name", Property.Type.String, true));
+      receiver.register("Transform", new Property(this, "Matrix", Property.Type.Transform, false));
+      receiver.register("Mesh", new Property(this, "MeshId", Property.Type.Id, true,
+          new IdList<Mesh>(context.getMeshes(), true), new NullOffsetter()));
+      receiver.register("Material", new Property(this, "MaterialId", Property.Type.Id, true,
+          new IdList<Material>(context.getMaterials(), true), new NullOffsetter()));
+      receiver.register("Shader", new Property(this, "ShaderId", Property.Type.Id, true,
+          new IdList<Shader>(context.getShaders(), true), new NullOffsetter()));
+    }
+    catch (NoSuchFieldException | NoSuchMethodException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
   }
 
   @JsonProperty("name")
