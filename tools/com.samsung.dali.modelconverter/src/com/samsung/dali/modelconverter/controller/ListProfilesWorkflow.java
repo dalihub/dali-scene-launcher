da229b88554580d8dfefdffa37707c9801401017
György Straub <g.straub@partner.samsung.com>
2017-12-11 15:42:11 +0000

    Added ManageProfilesHandler.
    - Renamed SelectCertificateDialog to ManageProfilesDialog.
    - Added LoggingProcessRunner, TextAppendHelper, ListProfilesWorkflow and OutputPart.
    - Updated Application.e4xmi with a menu and option for the handler + OutputPart.
    
    Change-Id: I53c1f4ba05110a65af759f7d65a6a89462bce30d

---
+++ tools/com.samsung.dali.modelconverter/src/com/samsung/dali/modelconverter/controller/ListProfilesWorkflow.java
@@ -0,0 +1,61 @@
+package com.samsung.dali.modelconverter.controller;
+
+/*
+ * Copyright (c) 2017 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+import java.util.List;
+
+import org.eclipse.swt.widgets.Shell;
+
+import com.samsung.dali.modelconverter.data.GlobalData;
+import com.samsung.dali.modelconverter.view.parts.GlobalParts;
+import com.samsung.dali.modelconverter.view.parts.OutputPart;
+
+public class ListProfilesWorkflow {
+
+  public static void execute(Shell shell, List<String> outProfiles) {
+    assert outProfiles != null;
+
+    OutputPart op = GlobalParts.getOutputPart();
+    LoggingProcessRunner lpr = LoggingProcessRunner.create(shell.getDisplay(), op.getText());
+
+    LoggingProcessRunner.Parser parser = new LoggingProcessRunner.Parser() {
+
+      @Override
+      public void parseLine(String line) {
+        if (mCare) {
+          if (!line.isEmpty()) {
+            line = line.trim();
+            // The profile might be active, in which case there will be a few spaces and an O following it. Then, go figure, more spaces.
+            if (line.endsWith("O")) {
+              line = line.substring(0, line.length() - 1).trim();
+            }
+            outProfiles.add(line);
+          }
+        }
+        else {
+          mCare = line.startsWith("[Profile Name]");
+        }
+      }
+
+      private boolean mCare = false;
+    };
+
+    lpr.addCommand(GlobalData.get().getTizenPath() + " security-profiles list", parser)
+      .run();
+  }
+}
