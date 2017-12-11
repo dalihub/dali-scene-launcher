05b907cb81a0b9ad13caa537b8062d29e7877234
György Straub <g.straub@partner.samsung.com>
2017-12-11 15:42:11 +0000

    Added ManageProfilesHandler.
    - Renamed SelectCertificateDialog to ManageProfilesDialog.
    - Added LoggingProcessRunner, TextAppendHelper, ListProfilesWorkflow and OutputPart.
    - Updated Application.e4xmi with a menu and option for the handler + OutputPart.
    
    Change-Id: I53c1f4ba05110a65af759f7d65a6a89462bce30d

---
+++ tools/com.samsung.dali.modelconverter/src/com/samsung/dali/modelconverter/controller/handlers/ManageProfilesHandler.java
@@ -0,0 +1,57 @@
+package com.samsung.dali.modelconverter.controller.handlers;
+
+/*
+ * Copyright (c) 2017 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+import java.util.ArrayList;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.swt.widgets.Shell;
+
+import com.samsung.dali.modelconverter.controller.ListProfilesWorkflow;
+import com.samsung.dali.modelconverter.data.Settings;
+import com.samsung.dali.modelconverter.view.dialogs.ManageProfilesDialog;
+
+public class ManageProfilesHandler {
+
+  @Execute
+  void execute(Shell shell) {
+
+    // Get list of security profiles.
+    ArrayList<String> securityProfiles = new ArrayList<String>();
+    ListProfilesWorkflow.execute(shell, securityProfiles);
+
+    // Create and populate dialog.
+    ManageProfilesDialog manageDialog = new ManageProfilesDialog(shell);
+    manageDialog.setProfiles(securityProfiles);
+
+    String activeProfile = Settings.Get().getString(ACTIVE_PROFILE);
+    int selection = securityProfiles.indexOf(activeProfile);
+    if (selection == -1) {
+      selection = 0;
+    }
+    manageDialog.selectProfile(selection);
+
+    // Get user input.
+    if (manageDialog.open() == IDialogConstants.OK_ID) {
+      Settings.Get().setString(ACTIVE_PROFILE, manageDialog.getResult());
+    }
+  }
+
+  private static final String ACTIVE_PROFILE = "ACTIVE_PROFILE";
+}
