fdfdb6fbbb4e03732329bf3863ce788e517ab736
greynaga <g.reynaga@samsung.com>
2018-01-12 11:09:34 +0000

    Set initial environment orientation bug
    Change-Id: Ic6413f4c5547b7ce6367146bb579ffcf764bd0d0

--- scene-launcher/scene-launcher-example.cpp
+++ scene-launcher/scene-launcher-example.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2018 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -353,8 +353,7 @@ public:
     {
       Matrix cubeMatrix;
       uniformValue.Get( cubeMatrix );
-      cubeMatrix.Transpose();
-      mCubeOrientation = camOrientation * Quaternion( cubeMatrix );
+      mCubeOrientation = Quaternion(cubeMatrix);
     }
 
     Actor skyBoxActor = mSkybox.GetActor();
