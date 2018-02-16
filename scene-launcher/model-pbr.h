133238911a9784677861ca5826bf9e5eb4e78ca2
György Straub <g.straub@partner.samsung.com>
2018-02-16 17:43:21 +0000

    Added functionality for cloning Actor hierarchy and Renderers as an enabler for multi-pass rendering of the same scene with different options.
    Change-Id: I1bb2e686be6dbf1f0570066f84c40d8030a3d2a0

--- scene-launcher/model-pbr.h
+++ scene-launcher/model-pbr.h
@@ -26,6 +26,9 @@
 #include <dali/public-api/rendering/shader.h>
 #include <dali/public-api/rendering/texture-set.h>
 
+// INTERNAL INCLUDES
+#include "utils.h"
+
 using namespace Dali;
 
 namespace SceneLauncher
@@ -37,6 +40,12 @@ struct Asset;
 class ModelPbr
 {
 public:
+  enum CloneOptions: SceneLauncher::CloneOptions::Type
+  {
+    GET_SHADERS = SceneLauncher::CloneOptions::FIRST_USER_OPTION,
+    GET_SKYBOX_TEXTURE = SceneLauncher::CloneOptions::FIRST_USER_OPTION << 1,
+  };
+
   /**
    * @brief Constructor.
    * Does nothing.
@@ -116,6 +125,23 @@ public:
    * @return Skybox texture.
    */
   Texture GetSkyboxTexture();
+
+  /**
+   * @brief Clone the actors and their renderers. Geometry and Shader references will be shared.
+   * Optionally, the vector of shader references and the skybox texture may also be shared.
+   * @param other The ModelPbr instance to duplicate into.
+   * @param cloneOptions A bitmask constructed from any combination of ::CloneOptions::Values and
+   *    ModelPbr::CloneOptions values.
+   */
+  void Duplicate(ModelPbr& other, SceneLauncher::CloneOptions::Type cloneOptions = SceneLauncher::CloneOptions::NONE) const;
+
+  /**
+   * @brief Adds the given texture to each texture set that any renderer on any actor might
+   * have in the hierarchy.
+   * @param texture Texture to attach.
+   * @param sampler Sampler to associate with @a texture.
+   */
+  void AttachTexture(Texture texture, Sampler sampler);
 
   /**
    * @brief Create a node with actor, mesh and renderer
