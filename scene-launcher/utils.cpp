9969a5d570da462b746205ad1df7d842a63da370
György Straub <g.straub@partner.samsung.com>
2018-02-16 17:43:21 +0000

    Added functionality for cloning Actor hierarchy and Renderers as an enabler for multi-pass rendering of the same scene with different options.
    Change-Id: I1bb2e686be6dbf1f0570066f84c40d8030a3d2a0

---
+++ scene-launcher/utils.cpp
@@ -0,0 +1,128 @@
+/*
+ * Copyright (c) 2018 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+#include "utils.h"
+#include <vector>
+
+using namespace Dali;
+
+Renderer CloneRenderer(Renderer r, CloneOptions::Type cloneOptions)
+{
+  Geometry g = r.GetGeometry();
+  Shader s = MaskMatch(cloneOptions, CloneOptions::OMIT_SHADER) ? Shader() : r.GetShader();
+  Renderer rNew = Renderer::New(g, s);
+
+  // Copy properties.
+  Property::IndexContainer indices;
+  r.GetPropertyIndices(indices);
+
+  for (auto& i: indices)
+  {
+    rNew.SetProperty(i, r.GetProperty(i));
+  }
+
+  // Copy texture references (and create TextureSet, if there's any textures).
+  TextureSet ts = r.GetTextures();
+  if(!MaskMatch(cloneOptions, CloneOptions::OMIT_TEXTURES) && ts)
+  {
+    TextureSet tsNew = TextureSet::New();
+    for(unsigned int i = 0; i < tsNew.GetTextureCount(); ++i)
+    {
+      tsNew.SetTexture(i, ts.GetTexture(i));
+      tsNew.SetSampler(i, ts.GetSampler(i));
+    }
+    rNew.SetTextures(tsNew);
+  }
+
+  return rNew;
+}
+
+Actor CloneActor(Actor a, CloneOptions::Type cloneOptions)
+{
+  Actor aNew = Actor::New();
+  aNew.SetName(a.GetName() + "-cloned");
+
+  // Copy properties.
+  Property::IndexContainer indices;
+  a.GetPropertyIndices(indices);
+
+  for (auto& i: indices)
+  {
+    aNew.SetProperty(i, a.GetProperty(i));
+  }
+
+  // Clone renderers.
+  if (!MaskMatch(cloneOptions, CloneOptions::OMIT_RENDERERS))
+  {
+    for(unsigned int i = 0; i < a.GetRendererCount(); ++i)
+    {
+      Renderer rNew = CloneRenderer(a.GetRendererAt(i), cloneOptions);
+      aNew.AddRenderer(rNew);
+    }
+  }
+
+  if (!MaskMatch(cloneOptions, CloneOptions::NO_RECURSE))
+  {
+    // Recurse into children.
+    for(unsigned int i = 0; i < a.GetChildCount(); ++i)
+    {
+      Actor newChild = CloneActor(a.GetChildAt(i), cloneOptions);
+      aNew.Add(newChild);
+    }
+  }
+  return aNew;
+}
+
+/**
+* @brief Load a shader source file
+* @param[in] The path of the source file
+* @param[out] The contents of file
+* @return True if the source was read successfully
+*/
+bool LoadShaderCode( const std::string& fullpath, std::vector<char>& output )
+{
+  FILE* f = fopen( fullpath.c_str(), "rb" );
+
+  if( NULL == f )
+  {
+    return false;
+  }
+
+  fseek( f, 0, SEEK_END );
+  size_t size = ftell( f );
+  fseek( f, 0, SEEK_SET );
+  output.resize( size + 1 );
+  std::fill( output.begin(), output.end(), 0 );
+  ssize_t result = fread( output.data(), size, 1, f );
+  fclose( f );
+  return ( result >= 0 );
+}
+
+
+Shader CreateShader( const std::string& shaderVertexFileName, const std::string& shaderFragFileName )
+{
+  Shader shader;
+  std::vector<char> bufV, bufF;
+
+  if( LoadShaderCode( shaderVertexFileName.c_str(), bufV ) )
+  {
+    if( LoadShaderCode( shaderFragFileName.c_str(), bufF ) )
+    {
+      shader = Shader::New( bufV.data() , bufF.data(), Shader::Hint::MODIFIES_GEOMETRY );
+    }
+  }
+  return shader;
+}
