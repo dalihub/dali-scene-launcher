6519762a4fff307c50cc5e7b16c6c2cfe795b4c3
György Straub <g.straub@partner.samsung.com>
2018-08-22 12:03:17 +0100

    Updated utils.h with new functionality.
    - Added ExceptionFlinger;
    - Added LoadTextFile() and removed LoadShaderCode();
    - Added MakeTexturedQuad... family of functions;
    - Fixed the copying of properties in CloneRenderers/Actors();
    
    Change-Id: Ibf9ae913dc54ace1f8acc139726438822dc6a7b1

--- scene-launcher/utils.h
+++ scene-launcher/utils.h
@@ -19,16 +19,65 @@
 
 #include <dali/public-api/actors/actor.h>
 #include <dali/public-api/rendering/renderer.h>
+#include <dali/public-api/common/dali-common.h>
+#include <sstream>
 #include <cctype>
 
 namespace SceneLauncher
 {
+
+class ExceptionFlinger
+{
+public:
+  ExceptionFlinger(const char* location)
+  :  mLocation(location),
+    mStreamBuffer(sMessageBuffer, sizeof(sMessageBuffer) - 1),
+    mStream(&mStreamBuffer)
+  {}
+
+  ~ExceptionFlinger() noexcept(false)
+  {
+    throw Dali::DaliException(mLocation, sMessageBuffer);
+  }
+
+  template <typename T>
+  ExceptionFlinger& operator<<(const T& rhs)
+  {
+    mStream << rhs;
+    return *this;
+  }
+
+private:
+  struct StreamBuffer : std::basic_streambuf<char>
+  {
+    StreamBuffer(char* buffer, size_t size)
+    {
+      setp(buffer, buffer + size);
+    }
+  };
+
+  static char sMessageBuffer[512];
+
+  const char* mLocation;
+  StreamBuffer mStreamBuffer;
+  std::ostream mStream;
+};
+
+///@return The @n th bit in a bitmask.
+constexpr size_t NthBit(size_t n) { return 1 << n; }
 
 ///@return Whether all of @a mask 's bits are set on @a value.
 inline
 bool MaskMatch(uint32_t value, uint32_t mask)
 {
   return (value & mask) == mask;
+}
+
+///@brief Convert a four-letter(, null-terminated) string literal into a uint32_t.
+inline
+constexpr uint32_t FourCC(const char(&fourCC)[5])
+{
+  return (fourCC[3] << 24) | (fourCC[2] << 16) | (fourCC[1] << 8) | fourCC[0];
 }
 
 ///@brief Insensitive case compare function.
@@ -56,6 +105,12 @@ bool CaseInsensitiveStringCompare( const std::string& a, const std::string& b )
   return result;
 }
 
+///@brief Attempts to load the contents of a text file; returns empty string on
+/// failure. A pointer to a boolean may be passed in @a fail; this will be set
+/// to true in case of failure (should only be checked if the returned string
+/// was empty()).
+std::string LoadTextFile(const char* path, bool* fail = nullptr);
+
 ///@brief Makes a number of calls to @a fn, passing to each one the given
 /// @a actor then each of its children, in depth-first traversal.
 ///@note @a fn must not change the actor hierarchy during traversal.
@@ -74,6 +129,19 @@ void VisitActor(Dali::Actor a, Func fn)
   }
 }
 
+///@brief Convenience function to set the given actor @a 's anchor point
+/// and parent origin to center.
+inline
+void SetActorCentered(Dali::Actor a)
+{
+  a.SetAnchorPoint(Dali::AnchorPoint::CENTER);
+  a.SetParentOrigin(Dali::ParentOrigin::CENTER);
+}
+
+///@brief Creates a copy of the given texture set, by creating a new instance and
+/// copying all Texture and Sampler references from it.
+Dali::TextureSet CloneTextures(Dali::TextureSet ts);
+
 namespace CloneOptions
 {
 using Type = uint32_t;
@@ -87,6 +155,7 @@ enum Values: Type
   OMIT_RENDERERS = 0x10,  // Do not clone Renderers (Actor).
   NO_RECURSE = 0x20,      // Do not clone children (Actor).
   CONSTRAIN_XFORM = 0x40, // Constrain the position and orientation of the clone to the original.
+  CONSTRAIN_VISIBILITY = 0x80, // Constrain the visibility of the clone to the original.
 
   // Local additions to CloneOptions should start at this value; consider all preceding ones reserved.
   FIRST_USER_OPTION = 0x100,
@@ -95,18 +164,16 @@ enum Values: Type
 }
 
 ///@brief Creates a new Renderer with all conceivable properties of the given
-/// Renderer @a r. A new TextureSet will be created if needed, but all Texture,
-/// Shader and Geometry references are shared between @a r and the new
-/// instance. The creation of a TextureSet (and subsequent sharing of Texture
-/// references) may be omitted using CloneOptions::OMIT_TEXTURES. The sharing
-/// of the Shader reference maybe omitted using CloneOptions::OMIT_SHADER.
+/// Renderer @a r. A new TextureSet will be created if the original had one and
+/// CloneOptions::OMIT_TEXTURES was not specified, but all Texture, Shader and
+/// Geometry references are shared between @a r and the new instance.
 ///@param r The Renderer to clone.
 ///@param cloneOptions A bitmask constructed from the combination of CloneOptions
 /// values (plus any user defined values).
 Dali::Renderer CloneRenderer(Dali::Renderer r, CloneOptions::Type cloneOptions = CloneOptions::NONE);
 
-///@brief Create a new actor with all conceivable properties (excluding actual
-/// Dali::Property-s at the moment) and Renderers of the given actor @a a.
+///@brief Create a new actor with all conceivable properties and Renderers of
+/// the given actor @a a.
 /// Will clone all children unless CloneOptions::NO_RECURSE was set.
 /// Will clone Renderers unless CloneOptions::OMIT_RENDERERS was set.
 ///@param a The Actor to clone.
@@ -119,13 +186,38 @@ Dali::Actor CloneActor(Dali::Actor a, CloneOptions::Type cloneOptions = CloneOpt
 /// some value based on the type; see dali/public-api/object/property-index-range.h
 void DumpProperties(Dali::Handle object);
 
-/**
-* @brief Load vertex and fragment shader source
-* @param[in] shaderVertexFileName is the filepath of Vertex shader
-* @param[in] shaderFragFileName is the filepath of Fragment shader
-* @return the Dali::Shader object
-*/
+///@brief Load vertex and fragment shader source
+///@param[in] shaderVertexFileName is the filepath of Vertex shader
+///@param[in] shaderFragFileName is the filepath of Fragment shader
+///@return the Dali::Shader object.
 Dali::Shader CreateShader(const std::string& vertexPath, const std::string& fragmentPath);
+
+namespace TexturedQuadOptions
+{
+using Type = uint32_t;
+
+enum Values : Type
+{
+  NONE = 0x00,
+  FLIP_VERTICAL = 0x01,
+  GENERATE_BARYCENTRICS = 0x02,
+};
+}
+
+///@brief Makes... geometry for a textured quad.
+Dali::Geometry MakeTexturedQuadGeometry(TexturedQuadOptions::Type options = TexturedQuadOptions::NONE);
+
+///@brief Makes... a textured quad renderer. With back face culling enabled.
+///@param texture The texture to use as texture 0 of a set we're creating.
+///@param shader The shader to use with the renderer.
+Dali::Renderer MakeTexturedQuadRenderer(Dali::Texture texture, Dali::Shader shader,
+  TexturedQuadOptions::Type options = TexturedQuadOptions::NONE);
+
+///@brief Makes... a textured quad actor. Centered. With back face culling enabled.
+///@param texture The texture to use as texture 0 of a set we're creating.
+///@param shader The shader to use with the renderer.
+Dali::Actor MakeTexturedQuadActor(Dali::Texture texture, Dali::Shader shader,
+  TexturedQuadOptions::Type options = TexturedQuadOptions::NONE);
 
 }
 
